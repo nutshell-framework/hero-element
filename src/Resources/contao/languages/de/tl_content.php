7212dafcc8915414cc381dd00a27152d8142279e
Dennis Erdmann <dennis@erdmann-freunde.de>
2022-01-07 16:32:10 +0100

    add translations

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -16,6 +16,9 @@ declare(strict_types=1);
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_content']['addText'] = ['Einen Text hinzufügen', 'Den Inhaltselement einen Text hinzufügen'];
+$GLOBALS['TL_LANG']['tl_content']['background_legend'] = ['Hintergrundbild-Einstellungen'];
+$GLOBALS['TL_LANG']['tl_content']['addBackgroundImage'] = ['Einen Hintergrundbild hinzufügen', 'Dem Inhaltselement ein Hintergrundbild hinzufügen'];
+$GLOBALS['TL_LANG']['tl_content']['heroBackgroundImage'] = ['Quelldatei', 'Bitte wählen Sie eine Datei oder einen Ordner aus der Dateiübersicht'];
 $GLOBALS['TL_LANG']['tl_content']['link_primary_legend'] = 'Primäre Verlinkung';
 $GLOBALS['TL_LANG']['tl_content']['link_secondary_legend'] = 'Sekundäre Verlinkung';
 $GLOBALS['TL_LANG']['tl_content']['linkClass'] = ['Link-Klasse', 'Hier können Sie eine Link-Klasse vergeben, z.B. btn btn--primary'];
